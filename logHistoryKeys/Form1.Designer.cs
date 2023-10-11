10d8c769a04e4b7dc0cc7abb1674609934b5ccb1
mainfranc <igoromskiy@gmail.com>
2023-10-11 17:16:15 +0300

    Добавьте файлы проекта.

---
+++ logHistoryKeys/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace logHistoryKeys
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Путь к текстовому файлу";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(170, 24);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(427, 20);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(31, 86);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(135, 86);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 149);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ logHistoryKeys/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace logHistoryKeys
+{
+    public partial class Form1 : Form
+    {
+        enum HookType
+        {
+            WH_KEYBOARD_LL = 13,
+            WH_MOUSE_LL = 14
+        }
+
+        private bool isLogging = false;
+        private string logFilePath;
+        private HookProc keyboardHookDelegate;
+        private HookProc mouseHookDelegate;
+        private IntPtr keyboardHookId = IntPtr.Zero;
+        private IntPtr mouseHookId = IntPtr.Zero;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!isLogging && this.txtPath.Text != "")
+            {
+                logFilePath = this.txtPath.Text;
+                keyboardHookDelegate = KeyboardHookProc;
+                mouseHookDelegate = MouseHookProc;
+                keyboardHookId = SetHook(HookType.WH_KEYBOARD_LL, keyboardHookDelegate);
+                mouseHookId = SetHook(HookType.WH_MOUSE_LL, mouseHookDelegate);
+                isLogging = true;
+                btnStart.Enabled = false;
+                btnStop.Enabled = true;
+            }
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (isLogging && this.txtPath.Text != "")
+            {
+                UnhookWindowsHookEx(keyboardHookId);
+                UnhookWindowsHookEx(mouseHookId);
+                isLogging = false;
+                btnStart.Enabled = true;
+                btnStop.Enabled = false;
+            }
+        }
+
+        private IntPtr SetHook(HookType hookType, HookProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx((int)hookType, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private IntPtr KeyboardHookProc(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                char keyChar = (char)vkCode;
+
+                if (isLogging)
+                {
+                    using (StreamWriter logFile = new StreamWriter(logFilePath, true))
+                    {
+                        logFile.WriteLine("|" + DateTime.Now + "|:    " + keyChar);
+                    }
+                }
+            }
+
+            return CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
+        }
+
+        private IntPtr MouseHookProc(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && (wParam == (IntPtr)WM_LBUTTONDOWN || wParam == (IntPtr)WM_RBUTTONDOWN))
+            {
+                int x = Marshal.ReadInt32(lParam);
+                int y = Marshal.ReadInt32(lParam + 4);
+
+                if (isLogging)
+                {
+                    using (StreamWriter logFile = new StreamWriter(logFilePath, true))
+                    {
+                        logFile.WriteLine("|" + DateTime.Now  + "|:    " + $"Mouse click at X: {x}, Y: {y}");
+                    }
+                }
+            }
+
+            return CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
+        }
+
+        const int WM_KEYDOWN = 0x0100;
+        const int WM_LBUTTONDOWN = 0x0201;
+        const int WM_RBUTTONDOWN = 0x0204;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+    }
+}
